362d6d3ed0f41f4707891e415d9c7a65ec0b7f30
Ricky Nyairo <rickynyairo@gmail.com>
2023-10-10 11:24:47 +0300

    print pinned repos

--- tests/github.spec.ts
+++ tests/github.spec.ts
@@ -5,5 +5,11 @@ test('can get pinned repositories', async ({ page }) => {
   const profilePage = new GithubProfilePage(page, 'rickynyairo');
   await profilePage.goto();
   const pinnedRepositories = await profilePage.getPinnedRepositories();
-  expect(pinnedRepositories.length).toBeGreaterThan(1);
+  // print repo names
+  for (const repo of pinnedRepositories) {
+    const title  = repo.locator('.text-bold').first();
+    console.log(await title.innerText())
+  }
+  // assertion
+  expect(pinnedRepositories.length).toBeGreaterThan(0);
 });
